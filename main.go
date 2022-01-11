6ae0c542ba8f7d44b2e218b471617d54f8b66661
keys4words <keys4words@gmail.com>
2022-01-11 15:34:46 +0300

    add http method handler

--- main.go
+++ main.go
@@ -2,18 +2,35 @@ package main
 
 import (
 	"fmt"
+	"github.com/gorilla/mux"
 	"log"
 	"net/http"
-	"github.com/gorilla/mux"
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Welcome to REST API!")
 }
 
+func allcources(w http.ResponseWriter, r *http.Request) {
+	kv := r.URL.Query()
+	for k, v := range kv {
+		fmt.Println(k, v)
+	}
+}
+
+func course(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	fmt.Fprintf(w, "Detail for course "+params["courseid"])
+	fmt.Fprintf(w, "\n")
+	fmt.Fprintf(w, r.Method)
+}
+
 func main() {
 	router := mux.NewRouter()
 	router.HandleFunc("/api/v1/", home)
+
+	router.HandleFunc("/api/v1/courses", allcources)
+	router.HandleFunc("/api/v1/courses/{courseid}", course).Methods("GET", "PUT", "POST", "DELETE")
 
 	fmt.Println("Listening on port 5000")
 	log.Fatal(http.ListenAndServe(":5000", router))

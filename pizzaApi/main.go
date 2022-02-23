8e3b295e5a7ae9dfb8b0048ce64c3dc733b55b6a
keys4words <keys4words@gmail.com>
2022-02-23 22:27:54 +0300

    finished pizzaApi handlers

--- pizzaApi/main.go
+++ pizzaApi/main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
 	"net/http"
+	"strconv"
 
 	"github.com/gorilla/mux"
 )
@@ -12,11 +14,37 @@ var (
 	db   []Pizza
 )
 
+func init() {
+	pizza1 := Pizza{
+		ID:      1,
+		Dimeter: 22,
+		Price:   500.54,
+		Title:   "Pepperoni",
+	}
+	pizza2 := Pizza{
+		ID:      2,
+		Dimeter: 22,
+		Price:   680.54,
+		Title:   "BBQ",
+	}
+	pizza3 := Pizza{
+		ID:      3,
+		Dimeter: 22,
+		Price:   450.04,
+		Title:   "4Cheese",
+	}
+	db = append(db, pizza1, pizza2, pizza3)
+}
+
 type Pizza struct {
 	ID      int     `json:"id"`
 	Dimeter int     `json:"diameter"`
 	Price   float64 `json:"price"`
 	Title   string  `json:"title"`
+}
+
+type ErrorMessage struct {
+	Message string `json:"message"`
 }
 
 func FindPizzaById(id int) (Pizza, bool) {
@@ -33,10 +61,33 @@ func FindPizzaById(id int) (Pizza, bool) {
 }
 
 func GetAllPizzas(writer http.ResponseWriter, request *http.Request) {
+	writer.Header().Set("Content-Type", "application/json")
+	log.Println("Show all pizzas")
+	writer.WriteHeader(200)
+	json.NewEncoder(writer).Encode(db)
 
 }
 func GetPizzaById(writer http.ResponseWriter, request *http.Request) {
-
+	writer.Header().Set("Content-Type", "application/json")
+	vars := mux.Vars(request)
+	id, err := strconv.Atoi(vars["id"])
+	if err != nil {
+		log.Println("client try use invalid id params", err)
+		msg := ErrorMessage{Message: "id must be int type"}
+		writer.WriteHeader(400)
+		json.NewEncoder(writer).Encode(msg)
+		return
+	}
+	log.Println("Trying to send to client pizza with id=", id)
+	pizza, ok := FindPizzaById(id)
+	if ok {
+		writer.WriteHeader(200)
+		json.NewEncoder(writer).Encode(pizza)
+	} else {
+		writer.WriteHeader(404)
+		msg := ErrorMessage{Message: "pizza with this id doesn't exist"}
+		json.NewEncoder(writer).Encode(msg)
+	}
 }
 
 func main() {

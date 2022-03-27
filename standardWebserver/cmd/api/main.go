d2ba1c8c525640c21be159ab1553b82ef9dc6640
keys4words <keys4words@gmail.com>
2022-03-27 18:22:41 +0300

    add possibility to load config from .env

--- standardWebserver/cmd/api/main.go
+++ standardWebserver/cmd/api/main.go
@@ -3,26 +3,41 @@ package main
 import (
 	"flag"
 	"log"
+	"os"
 
 	"github.com/BurntSushi/toml"
+	"github.com/joho/godotenv"
 	"github.com/keys4words/go-restfull_api/standardWebserver/internal/app/api"
 )
 
 var (
 	configPath string
+	format     string
 )
 
 func init() {
-	flag.StringVar(&configPath, "path", "configs/api.toml", "path to config file in toml format")
+	flag.StringVar(&format, "format", ".toml", "format of config file")
+	flag.StringVar(&configPath, "path", "configs/api.toml", "path to config file in .toml format")
 }
 
 func main() {
 	flag.Parse()
 	log.Println("It works")
 	config := api.NewConfig()
-	_, err := toml.DecodeFile(configPath, config)
-	if err != nil {
-		log.Println("cannot parse toml file, use default value ", err)
+
+	if format == ".toml" {
+		_, err := toml.DecodeFile(configPath, config)
+		if err != nil {
+			log.Println("cannot parse toml file, use default value ", err)
+		}
+	}
+	if format == ".env" {
+		err := godotenv.Load(configPath)
+		if err != nil {
+			log.Println("cannot parse .env file, use default value ", err)
+		}
+		config.BindAddr = os.Getenv("bind_addr")
+		config.LoggerLevel = os.Getenv("logger_level")
 	}
 
 	server := api.New(config)
--- standardWebserver/go.mod
+++ standardWebserver/go.mod
@@ -7,6 +7,7 @@ require (
 	github.com/auth0/go-jwt-middleware v1.0.1 // indirect
 	github.com/form3tech-oss/jwt-go v3.2.5+incompatible // indirect
 	github.com/gorilla/mux v1.8.0 // indirect
+	github.com/joho/godotenv v1.4.0 // indirect
 	github.com/lib/pq v1.10.4 // indirect
 	github.com/sirupsen/logrus v1.8.1 // indirect
 	golang.org/x/sys v0.0.0-20220224120231-95c6836cb0e7 // indirect
